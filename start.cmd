af1f21d382536cd51afbe041260be381b430e6b9
remote_vase <web.articlerequests@gmail.com>
2016-11-28 20:46:28 -0500

    Create start.cmd

---
+++ start.cmd
@@ -0,0 +1,24 @@
+@echo off
+TITLE PocketMine helper for PocketMine MP
+cd /d %~dp0
+
+if exist bin\php\php.exe (
+	set PHPRC=""
+	set PHP_BINARY=bin\php\php.exe
+) else (
+	set PHP_BINARY=php
+)
+
+if exist src\pocketmine\logger.php (
+	   set Logger_FILE=src\pocketmine\logger.php
+			) else (
+		        echo "[ERROR] Couldn't find a valid helper installation."
+		        pause
+		        exit 8
+		    )
+
+if exist bin\mintty.exe (
+	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "PocketMine Helper" -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
+) else (
+	%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
+)
