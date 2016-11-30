0646ffc535437d3612a5b498163e0a9d99363c60
remote_vase <web.articlerequests@gmail.com>
2016-11-29 20:01:47 -0500

    Update logger.php

--- src/pm-helper/logger.php
+++ src/pm-helper/logger.php
@@ -11,6 +11,11 @@ class logger{
 		echo "Welcome to the PocketMine Console Helper!\n";
 		echo "Choice 1: Install PocketMine";
 		echo "Choice 2: Update the PocketMine phar";
+		echo "Choice 3: Install a plugin";
+		echo "Choice 4: Download a Minecraft PC Edition world, and convert it for use on your server";
+		echo "Choice 5: Start your server normally";
+		echo "Choice 6: Start your server in a loop, so that it is online 24/7";
+		echo "Choice 7: Stop your server";
 		
 		echo "\n[?] " . $this->lang->accept_license . " (y/N): ";
 		if(strtolower($this->getInput("n")) != "y"){
